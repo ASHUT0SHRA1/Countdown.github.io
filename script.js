39f1e9ad471f6b02b2f7c5532cf1a4d3efa31e09
Ashutosh Rai <89987124+ASHUT0SHRA1@users.noreply.github.com>
2023-05-29 14:17:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdown = () => {
+    const currentDate = new Date();
+    const eventDate = new Date('2024-01-01');
+  
+    const totalSeconds = (eventDate - currentDate) / 1000;
+    
+
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+  };
+  
+  countdown();
+  
+  setInterval(countdown, 1000);
